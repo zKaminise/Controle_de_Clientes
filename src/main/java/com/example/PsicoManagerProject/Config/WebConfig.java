0e9f5cecbf325c06411060556c059d9d270e00bd
gabriel.kaminise <gabriel.kaminise@sankhya.com.br>
2024-12-27 16:10:22 -0300

    Ajustando Link CORS

--- src/main/java/com/example/PsicoManagerProject/Config/WebConfig.java
+++ src/main/java/com/example/PsicoManagerProject/Config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://controledeclientes-production.up.railway.app", "https://controle-de-clientes-front-end.vercel.app")
+                .allowedOrigins("http://localhost:5173", "https://controledeclientes-production.up.railway.app", "https://controleclientes.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
