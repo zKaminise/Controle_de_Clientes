bdeee5e6bb4f2e552e267aa1be324c1a479547da
gabriel.kaminise <gabriel.kaminise@sankhya.com.br>
2024-12-27 14:36:11 -0300

    Ajustando CORS

--- src/main/java/com/example/PsicoManagerProject/Config/WebConfig.java
+++ src/main/java/com/example/PsicoManagerProject/Config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // Substitua pelo URL do frontend
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
