6de48a8347c325c403d633c415bd0c54557ba887
gabriel.kaminise <gabriel.kaminise@sankhya.com.br>
2024-12-27 14:01:08 -0300

    Ajustei para Deploy

--- src/main/java/com/example/PsicoManagerProject/Repositorys/UserRepository.java
+++ src/main/java/com/example/PsicoManagerProject/Repositorys/UserRepository.java
@@ -2,7 +2,9 @@ package com.example.PsicoManagerProject.Repositorys;
 
 import com.example.PsicoManagerProject.Entitys.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     User findByUsername(String username);
     User findByResetPasswordToken(String token);
