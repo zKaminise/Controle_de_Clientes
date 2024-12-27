3ce0d7aa78d6178a7b9bdf2c261a251f35b84337
gabriel.kaminise <gabriel.kaminise@sankhya.com.br>
2024-12-27 14:56:36 -0300

    Ajustando CORS para comunicação

--- src/main/java/com/example/PsicoManagerProject/Config/WebConfig.java
+++ src/main/java/com/example/PsicoManagerProject/Config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:5173", "https://controledeclientes-production.up.railway.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
