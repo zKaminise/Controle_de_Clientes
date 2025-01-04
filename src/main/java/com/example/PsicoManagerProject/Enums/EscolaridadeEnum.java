06a5124d8886a3148ee53565dc02f53b16a9763c
gabriel.kaminise <gabriel.kaminise@sankhya.com.br>
2025-01-04 01:51:35 -0300

    Ajustes enum e template

--- src/main/java/com/example/PsicoManagerProject/Enums/EscolaridadeEnum.java
+++ src/main/java/com/example/PsicoManagerProject/Enums/EscolaridadeEnum.java
@@ -1,7 +1,7 @@
 package com.example.PsicoManagerProject.Enums;
 
 public enum EscolaridadeEnum {
-    ensinoSuperior, ensinoMedio, ensinoFundamentalIncompleto, ensinoFundamentalCompleto, ensinoMedioIncompleto,
+    ensinoFundamentalIncompleto, ensinoFundamentalCompleto, ensinoMedioIncompleto,
     ensinoMedioCompleto, ensinoSuperiorIncompleto, ensinoSuperiorCompleto, posGraduacaoIncompleta, posGraduacaoCompleta,
     mestradoIncompleto, mestradoCompleto, doutoradoIncompleto, doutoradoCompleto, posDoutoradoIncompleto, posDoutoradoCompleto
 }
